2be27b3f6c4e18812973f70ddd936f1a7348c230
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-01-20 19:13:55 -0500

    Edit to nav bar. Changed background on home screen, fixed error with projects not being mobile reponsive

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,15 @@
 $(document).ready(function() {
-  // Show Hide left-bar
-  $(".burger").on("click", function() {
-    if ($(".side-bar").css("display") === "none") {
-      $(".side-bar").show();
-      $(".main").css({ "padding-left": "85px" });
-    } else {
-      $(".side-bar").hide();
-      $(".main").css({ "padding-left": "0px" });
-    }
-  });
-
-  // Home button hide show
-  $(".home-btn").on("click", function() {
-    $(".font-icons").removeClass("font-icon-active");
-    $(".home-btn").addClass("font-icon-active");
-    $(".home").show();
-    $(".about").hide();
-    $(".portfolio").hide();
-    $(".contact").hide();
-  });
-
-  // About button hide show
-  $(".about-btn").on("click", function() {
-    $(".font-icons").removeClass("font-icon-active");
-    $(".about-btn").addClass("font-icon-active");
-    $(".home").hide();
-    $(".about").show();
-    $(".portfolio").hide();
-    $(".contact").hide();
-  });
-
   $(".about-contact-btn").on("click", function() {
     $(".about").hide();
     $(".contact").show();
   });
 
-  // Portfolio button hide show
-  $(".portfolio-btn").on("click", function() {
-    $(".font-icons").removeClass("font-icon-active");
-    $(".portfolio-btn").addClass("font-icon-active");
-    $(".home").hide();
-    $(".about").hide();
-    $(".portfolio").show();
-    $(".contact").hide();
-  });
+  const burger = document.querySelector(".burger-container");
+  const navButtons = document.querySelector(".nav-buttons");
 
-  // Contact button hide show
-  $(".contact-btn").on("click", function() {
-    $(".font-icons").removeClass("font-icon-active");
-    $(".contact-btn").addClass("font-icon-active");
-    $(".home").hide();
-    $(".about").hide();
-    $(".portfolio").hide();
-    $(".contact").show();
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active-burger");
+    navButtons.classList.toggle("active-nav-buttons")
   });
 
   $(".card").hover(
